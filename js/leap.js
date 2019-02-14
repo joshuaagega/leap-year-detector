e34168bca9549068beaf612a5965d9d48f5ee48a
joshua <javanmega0@gmail.com>
2019-02-14 15:57:03 +0300

    addition of true

--- js/leap.js
+++ js/leap.js
@@ -2,7 +2,9 @@ function findOut(){
 var input=document.getElementById('input').value;
 if (input % 4 == 0) {
   alert("true");
-}else if (input% 100 == 0 && input % 400 == 0) {}
+}else if (input% 100 == 0 && input % 400 == 0) {
+ alert("true")
+}
 
 
 
